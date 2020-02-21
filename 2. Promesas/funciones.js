28093e19561099d95d3f901685f4ed9c1d76cfc6
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-02-21 17:30:24 +0000

    Segunda version

--- 2. Promesas/funciones.js
+++ 2. Promesas/funciones.js
@@ -5,13 +5,20 @@ function add1(x) {
   return x + 1;
 }
 
+/**
+ * Returns true if x is even, false otherwise.
+ */
+function isEven(x) {
+  return x % 2 == 0;
+}
+
 console.log(add1(add1(5) + add1(10)));
 
 const x = [4, 1, 7, 2];
-const y = [];
+const y = x.map(add1);
+const z = x.filter(isEven);
+const a = x.filter(t => t > 4);
+const b = x.reduce((acum, elem) => acum + elem, 0);
 
-for (let e of x) {
-  y.push(add1(e));
-}
-
-console.log(y);
+console.log(x, y, z, a, b);
+x.forEach(e => console.log(e));
