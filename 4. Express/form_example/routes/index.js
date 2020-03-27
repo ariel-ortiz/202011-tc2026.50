f41804af2df863019c8f8d69727ca73e5d8b0263
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2020-03-27 17:11:28 +0000

    Usa variables de sesión para compartir datos entre peticiones

--- 4. Express/form_example/routes/index.js
+++ 4. Express/form_example/routes/index.js
@@ -6,21 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// BEWARE: This code is potentially dangerous. Using a global
-// variable like this may produce a race codition.
-let info;
-
 // Apply the "Redirect after Post" pattern,
 // a.k.a PRG (Post/Redierct/Get)
 // See https://en.wikipedia.org/wiki/Post/Redirect/Get
 router.post('/process', (req, res) => {
-  info = req.body.info || 'default value';
-  console.log(`info = "${ req.body.info }"`);
-  console.log(info);
+  req.session.info = req.body.info || 'default value';
   res.redirect('/process');  // Redirect using GET method
 });
 
 router.get('/process', (req, res) => {
+  const info = req.session.info;
   res.render('process', { info });
 });
 
